1a8d4474bfb77165ef347c83746d96edeab05f7c
GustavoE05 <gustavo-erheinheimer@educar.rs.gov.br>
2023-11-27 00:43:10 -0300

    login e cadastro

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'users/sessions#new'
+    get '/users/sign_in', to: 'devise/sessions#new'
+    post '/users/sign_in', to: 'devise/sessions#create'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  
+  get 'home/index'
+  root 'home#index'
+  
+  
+
+
+end
