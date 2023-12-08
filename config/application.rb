7c2d8b29f4fa48b5da320583236779e9e88f028e
GustavoE05 <gustavo-erheinheimer@educar.rs.gov.br>
2023-12-07 23:28:12 -0300

    hospedagem funcionando

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,9 @@ module Oficina
     config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
-    #
+
+    config.assets.precompile += %w( application.scss )
+    
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
